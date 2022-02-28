394caad24e3db52692eeb6e1dd30542a483b0916
lucasd <derhore.lucas@gmail.com>
2022-02-28 15:16:15 +0100

    User to Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
 
 User.destroy_all
+Article.destroy_all
 
 user = User.create(username: "lucas", password: "123456", age: 29)
+
+30.times do
+  article = Article.create(
+    title:Faker::Games::WorldOfWarcraft.hero,  
+    content:Faker::Games::WorldOfWarcraft.class_name,
+    user_id: 5 )
+end
+
+puts "LE SEED EST FAIT"
